9429a873ea36f68a78f3ef7a505965cb40b9756b
saul <solleedata@gmail.com>
2024-02-06 01:09:06 +0900

    + 호이스팅 여부

---
+++ hoisting.js
@@ -0,0 +1,11 @@
+// function 키워드 함수는 함수 전체를 호이스팅한다:
+main(); // 실행 전 호출 가능 main출력
+function main() {
+  console.log('main');
+};
+
+// 화살표 함수는 별도의 변수에 할당되어 호이스팅 영향 받지 않음:
+main2() // Uncaught TypeErro: main is not a function
+const main2 = () => {
+  console.log('main2');
+}
